d0bb5c77fb173011a24e83ffcbd6b9f4a42e1390
hajiiiin <101108440+hajiiiin@users.noreply.github.com>
2023-10-20 16:56:40 +0900

    home = index
    홈 화면은 index.html 수정하면 됩니다

--- app.js
+++ app.js
@@ -2,15 +2,27 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('public'));
-
 /*
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + '/main.html');
-  });
-  */
+const mongodbConnection = require("./config/mongodb-connection");
+
+let collection;
+app.listen(3000, async()=>{
+    console.log("Server started");
+    const MongoClient = await mongodbConnection();
+    collection = mongodbClient.db().collection("post");
+    console.log("MpngoDB connected");
+})
+*/
+
+app.use(express.static('public'));
 
 app.listen(PORT, (err) => {
   if (err) return console.log(err);
   console.log("The server is listening on port 3000");
 });
+
+app.get("/tourDetail/:id", async(req,res) => {
+  res.render("Detail", {
+    title: "test"
+  })
+})
