58d9df4e6b005d6f07959856cedac357ffc82c7e
hajiiiin <101108440+hajiiiin@users.noreply.github.com>
2023-10-23 00:14:56 +0900

    Update mongodb-connection.js

--- config/mongodb-connection.js
+++ config/mongodb-connection.js
@@ -1,6 +1,6 @@
 const {MongoClient} = require("mongodb")
 
-const uri = "";
+const uri = "mongodb+srv://tourList:3KnLVMvANkwmExyW@cluster0.ebnot2e.mongodb.net/Cluster0?retryWrites=true&w=majority";
 
 module.exports = function(callback){
     return MongoClient.connect(uri, callback)
