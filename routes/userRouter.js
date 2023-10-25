313723569f08f7aa7c6782f222eb66cc8d06ab70
hajiiiin <101108440+hajiiiin@users.noreply.github.com>
2023-10-25 13:55:07 +0900

    [FIX] 회원가입, 로그인
    수정 중....

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -7,9 +7,9 @@ const path = require('path');
 router.get('/')
 
 router.post('/signup', async (req, res) => {
-    console.log("확인")
     const { username, password, display_name } = req.body;
     const usersPath = path.join(__dirname, '../users.json');
+    console.log("입력 받은 값 : ");
     console.log(req.body);
     let users;
     try {
@@ -18,8 +18,13 @@ router.post('/signup', async (req, res) => {
     } catch (err) {
         users = [];
     }
-    console.log(users)
-    const existingUser = users.find(user => user.username === username);
+    console.log("users: ");
+    console.log(users);
+
+    const existingUser = users.some(user => user.username === username);
+    console.log("비교 결과 : ");
+    console.log(existingUser);
+
     if (existingUser) {
         // 회원가입 실패
         return res.status(400).json({ success: false, msg: 'User already exists! Please sign up again.' });
@@ -37,6 +42,8 @@ router.post('/signup', async (req, res) => {
 router.post('/login', async (req, res) => {
     const { username, password } = req.body; // 클라이언트로부터 받은 ID와 비밀번호
     const usersPath = path.join(__dirname, '../users.json');
+    console.log("입력 받은 값 : ");
+    console.log(req.body);
     
     let users;
     try {
@@ -46,7 +53,13 @@ router.post('/login', async (req, res) => {
         return res.status(500).json({ msg: 'Internal server error' });
     }
 
-    const existingUser = users.find(user => user.username === username && user.password === password);
+    console.log("users: ");
+    console.log(users);
+
+    const existingUser = users.some((user) => user.username === username && user.password === password);
+    console.log("비교 결과 : ");
+    console.log(existingUser);
+    
     if (existingUser) {
         // 로그인 성공
         return res.json({ http_status: 200, msg: 'Login successful!' });
